87e27a339d902ed44bd80946262ccb89ed02b5bd
vitorsilvalima <vitorsilvalima29@gmail.com>
2018-08-12 21:13:27 -0300

    chore: change webpack config files to work with ant design

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -15,6 +15,59 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+        },
+      },
+      {
+        test: /\.(js|jsx|mjs)$/,
+        include: paths.appSrc,
+        loader: require.resolve('babel-loader'),
+        options: {
+          plugins: [
+            ['import', [{ libraryName: 'antd', style: true }]], // Ant Design
+          ],
+          // This is a feature of `babel-loader` for webpack (not Babel itself).
+          // It enables caching results in ./node_modules/.cache/babel-loader/
+          // directory for faster rebuilds.
+          cacheDirectory: true,
+        },
+      },
+      {
+        test: /\.less$/,
+        use: [
+          require.resolve('style-loader'),
+          require.resolve('css-loader'),
+          {
+            loader: require.resolve('postcss-loader'),
+            options: {
+              ident: 'postcss', // https://webpack.js.org/guides/migrating/#complex-options
+              plugins: () => [
+                require('postcss-flexbugs-fixes'),
+                autoprefixer({
+                  browsers: [
+                    '>1%',
+                    'last 4 versions',
+                    'Firefox ESR',
+                    'not ie < 9', // React doesn't support IE8 anyway
+                  ],
+                  flexbox: 'no-2009',
+                }),
+              ],
+            },
+          },
+          {
+            loader: require.resolve('less-loader'),
+            options: {
+              // modifyVars: { "@primary-color": "#1DA57A" },
+              javascriptEnabled: true,
+            },
+          },
+        ],
+      },
+      {
         test: /\.css$/,
         enforce: 'pre',
         use: [
